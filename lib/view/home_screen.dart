8b261b1588e3ca3d0a6a2f94540b15b6e9d1416b
Raghvendra <raghvendrakt1@gmail.com>
2021-10-27 16:14:49 +0530

    getx implementation

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -19,57 +19,70 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
-              child: Obx(
-                () => catalogController.isLoadingComplete == false
-                    ? ListView.builder(
-                        itemCount: catalogController.products.length,
-                        itemBuilder: (context, index) {
-                          return Card(
-                            color: Colors.deepPurpleAccent,
-                            margin: const EdgeInsets.all(12),
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            '${catalogController.products[index].name}',
-                                            style: TextStyle(fontSize: 24),
-                                          ),
-                                          Text(
-                                              '${catalogController.products[index].desc}'),
-                                        ],
-                                      ),
-                                      Text(
-                                          '\$${catalogController.products[index].price}',
-                                          style: TextStyle(fontSize: 24)),
-                                    ],
-                                  ),
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      cartController.addToCart(
-                                          catalogController.products[index]);
-                                    },
-                                    child: Text('Add to Cart'),
-                                  ),
-                                ],
+              child: GetX<CatalogController>(
+                builder: (controller) {
+                  return controller.isLoadingComplete == false
+                      ? ListView.builder(
+                          itemCount: controller.products.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              color: Colors.deepPurpleAccent,
+                              margin: const EdgeInsets.all(12),
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              '${controller.products[index].name}',
+                                              style: TextStyle(fontSize: 24),
+                                            ),
+                                            Text(
+                                                '${controller.products[index].desc}'),
+                                          ],
+                                        ),
+                                        Text(
+                                            '\$${controller.products[index].price}',
+                                            style: TextStyle(fontSize: 24)),
+                                      ],
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        cartController.addToCart(
+                                            controller.products[index]);
+                                      },
+                                      child: Text('Add to Cart'),
+                                    ),
+                                    // IconButton(
+                                    //       icon: controller
+                                    //               .products[index].isFavorite.value
+                                    //           ? Icon(Icons.check_box_rounded)
+                                    //           : Icon(Icons
+                                    //               .check_box_outline_blank_outlined),
+                                    //       onPressed: () {
+                                    //         controller.products[index].isFavorite
+                                    //             .toggle();
+                                    //       },
+                                    //     )
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
-                        },
-                      )
-                    : Center(child: CircularProgressIndicator()),
+                            );
+                            ;
+                          },
+                        )
+                      : Center(child: CircularProgressIndicator());
+                },
               ),
             ),
-
             Obx(() => Text(
                 'Total Amount \$ ${cartController.totalAmount.toString()}')),
             //  GetX<CartController>(
